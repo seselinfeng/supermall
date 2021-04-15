6aaf0a8492364b9bf9d1edb6d0d2358168bfd5bf
seselinfeng <>
2021-04-15 09:20:03 +0800

    注释信息

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,50 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><template #center>购物街</template></nav-bar>
+    <!-- 轮播图 -->
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend :recommends="recommends"></home-recommend>
+
+  </div>
 </template>
 <script>
+import NavBar from "@/components/common/navbar/NavBar"
+import HomeSwiper from '@/views/home/childComps/HomeSwiper'
+import HomeRecommend from '@/views/home/childComps/HomeRecommend'
+import {getHomeMultidata} from "@/network/home.js"
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    HomeRecommend
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  }
 }
 </script>
 
 <style scope>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+  font-weight: 700;
+  /* 为了在使用原生滚动时，导航栏不随着内容滚动 */
+  /* position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9; */
+}
 </style>
---
+++ src/views/home/childComps/HomeRecommend.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="recommend">
+    <div v-for="(item, index) in recommends" :key="index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "HomeRecommend",
+  props: {
+    recommends: {
+      type: Array,
+      defalt() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scope>
+  #recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+    padding: 15px 0px 20px;
+    border-bottom: 8px #eee solid;
+  }
+  .recommend-item {
+    flex: 1;
+  }
+  .recommend-item img {
+    width: 80px;
+    height: 80px;
+    margin-bottom: 10px;
+  }
+</style>
